57e5126306760faa109a69af11cfccdf361b2b43
zhoujun <zhoujun1@icerno.com>
2017-11-17 10:55:01 +0800

    启动入口格式修改

--- src/main/java/com/longzheng/spring/boot/zhoujun/Application.java
+++ src/main/java/com/longzheng/spring/boot/zhoujun/Application.java
@@ -3,7 +3,6 @@ package com.longzheng.spring.boot.zhoujun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 //扫描:该包下的持久化类，主要是muybatis的持久化类;
 @MapperScan("com.longzheng.spring.boot.zhoujun.dao")
